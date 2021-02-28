97bbf8f451c6e548d8d55086a23e9b876db709a5
Lon Sagisawa <lon@sagisawa.me>
2021-02-28 14:35:25 +0900

    switch local fonts to fontsource

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,14 @@
 import React from "react"
 import Header from "./header"
 import Bio from "./bio"
+import "@exampledev/new.css"
+import "../custom.css"
+import "@fontsource/inter/400.css"
+import "@fontsource/inter/700.css"
+import "@fontsource/noto-sans-jp/400.css"
+import "@fontsource/noto-sans-jp/700.css"
+import "@fontsource/montserrat/700.css"
+import "@fontsource/fira-code"
 
 const Layout = ({ children }) => {
     return (
