66533c9fb0217863293720eb0b910b99b91312e7
Lon Sagisawa <lon@sagisawa.me>
2021-04-03 02:17:52 +0900

    tailwind basic conf

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
-  purge: [],
+  purge: [
+    './src/**/*.js',
+    './src/**/*.jsx',
+    './src/**/*.ts',
+    './src/**/*.tsx'
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      'sans': ['Inter', '"Hiragino Sans"', '"BIZ UDPGothic"', 'Meiryo', 'sans-serif'],
+      'heading': ['Montserrat', '"Hiragino Sans"', '"BIZ UDPGothic"', 'Meriyo', 'sans-serif'],
+    },
     extend: {},
   },
   variants: {
