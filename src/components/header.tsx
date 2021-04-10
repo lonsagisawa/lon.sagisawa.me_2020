aedbd40d4d914297153f92cdc0e05d9bf83a51b7
Lon Sagisawa <lon@sagisawa.me>
2021-04-10 22:16:59 +0900

    header underline fix

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,10 +4,8 @@ import { Link } from "gatsby"
 export default function Header() {
     return (
         <header className="py-8 sm:py-20 text-center sm:text-left">
-            <h1 className="
-                transition text-4xl font-heading hover:underline
-            ">
-                <Link to="/">Lon Sagisawa</Link>
+            <h1 className="text-4xl font-heading">
+                <Link to="/" className="transition border-b border-transparent hover:border-current">Lon Sagisawa</Link>
             </h1>
         </header>
     )
