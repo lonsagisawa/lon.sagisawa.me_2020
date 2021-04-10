b517b2a65023991e67fcf607ade1ba98307dace3
Lon Sagisawa <lon@sagisawa.me>
2021-04-10 22:17:24 +0900

    archive rework

--- src/components/post-link-minimal.tsx
+++ src/components/post-link-minimal.tsx
@@ -1,15 +1,24 @@
 import React from "react"
 import { Link } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 export default function PostLinkMinimal({ post }) {
     const { title, date } = post;
     const description = post.description.description;
     const pageLink = `/${post.year}/${post.month}/${post.slug}`
+    const coverImg = getImage(post.cover);
+    const coverTitle = post.cover.title;
 
     return (
-            <div className="my-4">
-                { date } - <Link to={ pageLink } className="underline transition hover:text-yellow-500">{ title }</Link>
-                <p className="my-1">{ description }</p>
-            </div>
+        <div className="transition bg-gray-50 shadow rounded-lg hover:shadow-lg hover:bg-gray-100">
+            <Link to={ pageLink } className="block min-h-full">
+                <GatsbyImage image={ coverImg } alt={ coverTitle } className="rounded-t-lg" imgClassName="block rounded-t-lg" />
+                <div className="p-4">
+                    <p className="font-heading font-bold text-xl">{ title }</p>
+                    <p className="text-xs mb-2">{ date }</p>
+                    <p className="text-sm">{ description }</p>
+                </div>
+            </Link>
+        </div>
     )
 }
--- src/pages/archive.tsx
+++ src/pages/archive.tsx
@@ -8,11 +8,12 @@ export default function Archive({ data }) {
     return (
     <Layout>
         <Head title="Lon Sagisawa" description="Private website of Lon Sagisawa" />
-        <h1 className="text-3xl">すべての記事</h1>
+        <h1 className="text-2xl sm:text-3xl mb-2 sm:mb-4">すべての記事</h1>
+        <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
           {data.allContentfulPost.edges.map(edge =>
             <PostLinkMinimal key={edge.node.slug} post={edge.node} />
           )}
-        
+        </div>
     </Layout>
     )
 }
@@ -27,10 +28,13 @@ export const query = graphql`
         year: date(formatString: "YYYY")
         month: date(formatString: "MM")
         cover {
+          gatsbyImageData(
+            layout: FULL_WIDTH,
+            quality: 90,
+            placeholder: NONE,
+            backgroundColor: "#434c5e"
+          )
           title
-          file {
-            url
-          }
         }
         description {
           description
