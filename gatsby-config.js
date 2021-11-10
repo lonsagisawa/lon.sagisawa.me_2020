6c3c24871547d43849612c8a5ce33d3f6723840a
Lon Sagisawa <lon@sagisawa.me>
2021-11-11 00:09:26 +0900

    🚀Gatsby v4

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,9 +39,6 @@ module.exports = {
     {
       resolve: `gatsby-transformer-remark`,
       options: {
-        commonmark: true,
-        footnotes: true,
-        pedantic: true,
         gfm: true,
         plugins: [
           {
