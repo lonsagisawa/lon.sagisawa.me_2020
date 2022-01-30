ccc0c79ceec9749a05f46ebccde43b5987dc1e29
Lon Sagisawa <lon@sagisawa.me>
2022-01-30 18:25:27 +0900

    tailwind v3 workaround

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: [
+  content: [
     './src/**/*.js',
     './src/**/*.jsx',
     './src/**/*.ts',
     './src/**/*.tsx'
   ],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
     fontFamily: {
       'sans': ['Rubik', 'Hiragino Sans', 'BIZ UDPGothic', 'Meiryo', 'sans-serif'],
