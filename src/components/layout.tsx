028b02461e699269397434ad28b9928f454dc4c6
Lon Sagisawa <lon@sagisawa.me>
2022-10-04 00:35:59 +0900

    simplify div hell

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,23 +1,31 @@
 import React from "react"
 import Header from "./header"
 import Bio from "./bio"
-import "@fontsource/rubik/400.css"
-import "@fontsource/rubik/700.css"
-import "@fontsource/oxanium/800.css"
+import { setup } from 'twind'
+import * as colors from 'twind/colors'
+import "@fontsource/inter/variable.css"
 import "@fontsource/jetbrains-mono"
 
 const Layout = ({ children }) => {
     return (
         <div>
-            <div>
-                <Header />
-                <main>{children}</main>
-            </div>
-            <div>
-                <Bio />
-            </div>
+            <Header />
+            <main>{children}</main>
+            <Bio />
         </div>
+                
     )
 }
+
+setup({
+    theme: {
+        fontFamily: {
+            sans: ['InterVariable', 'Hiragino Sans', 'BIZ UDPGothic', 'Meiryo', 'sans-serif'],
+        },
+        extend: {
+            colors,
+        },
+    },
+})
 
 export default Layout
