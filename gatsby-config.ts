88333bf5cfb390a3965bdfe246116a90f37e2af6
Lon Sagisawa <lon@sagisawa.me>
2022-10-22 23:00:47 +0900

    replace react to preact

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -26,6 +26,7 @@ const config: GatsbyConfig = {
     `gatsby-plugin-image`,
     `gatsby-plugin-gatsby-cloud`,
     `gatsby-plugin-emotion`,
+    `gatsby-plugin-preact`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
