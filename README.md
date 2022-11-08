1f5e2dc4841a2f453b1949a5e83441e4464742a9
Lon Sagisawa <lon@sagisawa.me>
2022-11-08 22:51:15 +0900

    here we go again...

--- README.md
+++ README.md
@@ -1,16 +1,6 @@
 # [lon.sagisawa.me](https://lon.sagisawa.me)
 
-My personal blog built on Gatsby 4.
-
-* Content delivered from Contentful.
-* Follows modern best practice as I could.
-* Designed for build on Gatsby Cloud, deploy to Vercel.
-* Utilize [Emotion](https://emotion.sh) for styling.
-* Installed TypeScript plugin for type safety, but not migrated yet.
-* Works as PWA.
-* Syntax highlight with Shiki.
-* RSS feed.
-* 95+ Lighthouse performance score. Reaches 100 in individual article page.
+My personal blog built on Gatsby 5. Still work in progress.
 
 ## Usage
 
@@ -28,5 +18,5 @@ npm run dev
 
 ## Thanks to
 
-* [Gatsby入門](https://zenn.dev/tomokiya/books/4b13342f6d878b93e06c) (Japanese) - This inspired me to try Gatsby again. This repo was basically result of this hands-on, but after my development and migration work to Gatsby v2, v3 and v4 it's been different.
+* [Gatsby入門](https://zenn.dev/tomokiya/books/4b13342f6d878b93e06c) (Japanese) - This inspired me to try Gatsby again. This repo was result of this hands-on before November 2022, but I built new blog from scratch.
 * Based on [gatsbyjs/gatsby-starter-hello-world](https://github.com/gatsbyjs/gatsby-starter-hello-world)
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,19 +1,49 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import Layout from "../components/layout"
-import Helmet from "../components/helmet"
+import { Link, HeadFC, PageProps } from "gatsby"
 
-const NotFound = () => {
+const pageStyles = {
+  color: "#232129",
+  padding: "96px",
+  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+}
+const headingStyles = {
+  marginTop: 0,
+  marginBottom: 64,
+  maxWidth: 320,
+}
+
+const paragraphStyles = {
+  marginBottom: 48,
+}
+const codeStyles = {
+  color: "#8A6534",
+  padding: 4,
+  backgroundColor: "#FFF4DB",
+  fontSize: "1.25rem",
+  borderRadius: 4,
+}
+
+const NotFoundPage: React.FC<PageProps> = () => {
   return (
-    <Layout>
-        <h1>404 Not Found</h1>
-        <Link to="/">トップページにもどる</Link>
-    </Layout>
+    <main style={pageStyles}>
+      <h1 style={headingStyles}>Page not found</h1>
+      <p style={paragraphStyles}>
+        Sorry 😔, we couldn’t find what you were looking for.
+        <br />
+        {process.env.NODE_ENV === "development" ? (
+          <>
+            <br />
+            Try creating a page in <code style={codeStyles}>src/pages/</code>.
+            <br />
+          </>
+        ) : null}
+        <br />
+        <Link to="/">Go home</Link>.
+      </p>
+    </main>
   )
 }
 
-export default NotFound
+export default NotFoundPage
 
-export const Head = () => (
-  <Helmet title="404 Not Found" description="ページが見つかりません" />
-)
+export const Head: HeadFC = () => <title>Not found</title>
