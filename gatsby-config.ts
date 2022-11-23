309ffda62353d1b21a50a37bfd8e7f3f2fca22d7
Lon Sagisawa <lon@sagisawa.me>
2022-11-24 02:30:50 +0900

    contentful fix

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -136,52 +136,27 @@ const config: GatsbyConfig = {
                 theme_color: "#f4d059",
                 display: "minimal-ui",
                 icon: "src/images/logo.png",
-                icons: [
-                    {
-                        src: "icons/icon-48.png",
-                        sizes: "48x48",
-                        type: "image/png",
-                    },
-                    {
-                        src: "icons/icon-72.png",
-                        sizes: "72x72",
-                        type: "image/png",
-                    },
-                    {
-                        src: "icons/icon-96.png",
-                        sizes: "96x96",
-                        type: "image/png",
-                    },
-                    {
-                        src: "icons/icon-144.png",
-                        sizes: "144x144",
-                        type: "image/png",
-                    },
-                    {
-                        src: "icons/icon-192.png",
-                        sizes: "192x192",
-                        type: "image/png",
-                    },
-                    {
-                        src: "icons/icon-256.png",
-                        sizes: "256x256",
-                        type: "image/png",
-                    },
-                    {
-                        src: "icons/icon-384.png",
-                        sizes: "384x384",
-                        type: "image/png",
-                    },
-                    {
-                        src: "icons/icon-512.png",
-                        sizes: "512x512",
-                        type: "image/png",
-                        purpose: "any maskable",
-                    },
-                ],
+                icon_options: {
+                    purpose: "any maskable",
+                },
+                cache_busting_mode: "none",
             },
         },
-        "gatsby-plugin-offline",
+        {
+            resolve: "gatsby-plugin-offline",
+            options: {
+                workboxConfig: {
+                    globPatterns: ["**/icons*"],
+                    runtimeCaching: [
+                        {
+                            /* Contentful fix */
+                            urlPattern: /^https?:.*\/page-data\/.*\.json/,
+                            handler: `NetworkFirst`,
+                        },
+                    ],
+                },
+            },
+        },
     ],
 }
 
