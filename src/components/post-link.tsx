40b1ea212bade49ba350e3cff52e541fbf67f0bc
Lon Sagisawa <lon@sagisawa.me>
2022-11-23 16:43:20 +0900

    color adjust

--- src/components/post-link.tsx
+++ src/components/post-link.tsx
@@ -11,21 +11,22 @@ interface PostLinkProps {
 const WrapperLink = styled(Link)({
     display: "block",
     borderWidth: "0.01px 0",
-    borderColor: "#ccc",
+    borderColor: "#8d8d99",
     borderStyle: "solid",
     transition: "all 100ms ease-out",
     ":hover": {
-        backgroundColor: "#f0f0f0",
+        backgroundColor: "#e6e9f2",
     },
     "@media (prefers-color-scheme: dark)": {
+        borderColor: "rgba(172, 176, 191, 0.5)",
         ":hover": {
-            backgroundColor: "#000",
+            backgroundColor: "#2d2d3d",
         },
     },
 })
 
 const Container = styled.div({
-    margin: "0.5rem 0",
+    margin: "0.5rem",
 })
 
 const Date = styled.p({
@@ -34,6 +35,7 @@ const Date = styled.p({
 
 const Title = styled.span({
     fontWeight: 700,
+    fontSize: "1.1rem",
 })
 
 const Description = styled.p({
